852d8bcd63527ba53b68fcd090169dc7b4ece40d
sualpemre <esualp95@gmail.com>
2021-05-26 03:14:40 +0300

    first commit

---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+
+using System;
+namespace UsersApi.Models
+{
+    public class User
+    {
+        public int Id{get;set;}
+
+        public string Name{get;set;}
+
+        public string Surname{get;set;}
+
+        public string Email{get;set;}
+
+        public string UserName{get;set;}
+
+        public string Password{get;set;}
+
+        public DateTime DateCreated{get;set;}
+    }
+}
